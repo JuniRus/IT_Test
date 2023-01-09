fdf38163b0a929dcf6327587046da2d4dd9af1b0
JuniRus <ruslanzubenok@gmail.com>
2023-01-09 20:21:04 +0300

    Завершение комментирования кода Form2.

--- IT_Test/Form2.cs
+++ IT_Test/Form2.cs
@@ -18,6 +18,8 @@ namespace IT_Test
         public int true_answear = 0;
         public int false_answear = 0;
 
+        // Статические переменные, которые будут содержать в себе
+        // результаты ответа на текущий вопрос.
         public static int True;
         public static int False;
 
@@ -33,16 +35,19 @@ namespace IT_Test
             false_answear += f;
         }
 
+        // Конструктор для кнопки "Назад" из следущей формы.
         public Form2()
         {
             InitializeComponent();
             // Явно выбрать изначально первый ответ.
             radioButton1.Checked = true;
 
+            // Взять результаты ответов предыдущей формы.
             true_answear = Form1.True;
             false_answear = Form1.False;
         }
 
+        // Нажатие на кнопку "Назад".
         private void button1_Click(object sender, EventArgs e)
         {
             // Спрятать текущий вопрос.
@@ -55,6 +60,7 @@ namespace IT_Test
             Close();
         }
 
+        // Нажатие на кнопку "Дальше".
         private void button2_Click(object sender, EventArgs e)
         {
             // Скрыть текущую форму.
@@ -70,6 +76,8 @@ namespace IT_Test
             else if (radioButton4.Checked)
                 false_answear++;
 
+            // Статические переменные, служащие для передачи информации
+            // об ответах текущего вопроса.
             True = true_answear;
             False = false_answear;
 
