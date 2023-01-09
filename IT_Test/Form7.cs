c0d625f78738a823f286b996e7812e84ee8a0a01
JuniRus <ruslanzubenok@gmail.com>
2023-01-09 20:46:05 +0300

    Завершение комментирования Form7.

--- IT_Test/Form7.cs
+++ IT_Test/Form7.cs
@@ -12,44 +12,59 @@ namespace IT_Test
 {
     public partial class Form7 : Form
     {
+        // Создание объекта предыдущего вопроса.
         Form6 form6 = new Form6();
-        public int true_answear;
-        public int false_answear;
+        // Переменные, хранящие в себе ответы.
+        public int true_answear = 0;
+        public int false_answear = 0;
 
+        // Статические переменные, которые будут содержать в себе
+        // результаты ответа на текущий вопрос.
         public static int True;
         public static int False;
 
+        // Конструктор для предыдущего вопроса кнопки "Дальше".
         public Form7(int t, int f)
         {
             InitializeComponent();
             radioButton1.Checked = true;
 
+            // Сохранение ответов из предыдущей формы.
             true_answear += t;
             false_answear += f;
         }
 
+        // Конструктор для кнопки "Назад" из следущей формы.
         public Form7()
         {
             InitializeComponent();
             radioButton1.Checked = true;
 
+            // Взять результаты ответов предыдущей формы.
             true_answear = Form6.True;
             false_answear = Form6.False;
         }
 
+        // Нажатие на кнопку "Назад".
         private void button1_Click(object sender, EventArgs e)
         {
+            // Спрятать текущий вопрос.
             Hide();
 
+            // Показать предыдущий вопрос.
             form6.ShowDialog();
 
+            // Закрыть окно текущего вопроса.
             Close();
         }
 
+        // Нажатие на кнопку "Дальше".
         private void button2_Click(object sender, EventArgs e)
         {
+            // Скрыть текущую форму.
             Hide();
 
+            // Изменение значений при выборе ответа.
             if (radioButton1.Checked)
                 true_answear++;
             else if (radioButton2.Checked)
@@ -59,12 +74,17 @@ namespace IT_Test
             else if (radioButton4.Checked)
                 false_answear++;
 
+            // Статические переменные, содержащие результаты
+            // ответов данного вопроса
             True = true_answear;
             False = false_answear;
 
+            // Передать ответы в следущий вопрос
             Form8 form8 = new Form8(true_answear, false_answear);
+            // Показать следущий вопрос.
             form8.ShowDialog();
 
+            // Закрыть текущий вопрос.
             Close();
         }
     }
